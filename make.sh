2aac3df40c1639b5d4af98ccbea89e718f8833f1
Shuai Yuan <ysh329@users.noreply.github.com>
2019-07-12 20:31:03 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 rm logging
-g++ logging.cc -std=c++11 -o logging && ./logging
+
+# enable macro NDEBUG
+echo "==== enable macro NDEBUG ===="
+g++ logging.cc -DNDEBUG -std=c++11 -o logging && ./logging
+echo 
+
+# disable macro NDEBUG
+echo "==== disable macro NDEBUG ===="
+g++ logging.cc -std=c++11 -o logging && ./logging 
