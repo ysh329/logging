383ea9149a8fcdbb8b63a0c6a118825be0fd5258
Shuai Yuan <ysh329@sina.com>
2019-07-12 23:50:49 +0800

    add

--- make_for_x86.sh
+++ make_for_x86.sh
@@ -3,9 +3,9 @@ rm logging
 
 # enable macro NDEBUG
 echo "==== enable macro NDEBUG ===="
-g++ logging.cc -DNDEBUG -std=c++11 -o logging && ./logging
+g++ example.cc -DNDEBUG -std=c++11 -o example && ./example
 echo 
 
 # disable macro NDEBUG
 echo "==== disable macro NDEBUG ===="
-g++ logging.cc -std=c++11 -o logging && ./logging 
+g++ example.cc -std=c++11 -o example && ./example
