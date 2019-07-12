4812ede72dc6e58464d8247ba8628795e917ca5f
Shuai Yuan <ysh329@sina.com>
2019-07-12 23:30:54 +0800

    add

--- logging.h
+++ logging.h
@@ -152,19 +152,3 @@ class VLogMessage {
   void operator=(const VLogMessage&) = delete;
 };
 
-
-int main() {
-
-  LOG(INFO) << "this is a LOG(INFO) message";
-  VLOG(3) << "this is a VLOG(3) message";
-
-  cout << "hello" << endl;
-  int a = 1;
-  LOG(INFO) << "a";
-  //LOG(FATAL) << "ccc";
-  float *b = nullptr;
-  //CHECK(b) << 123;
-  CHECK_EQ(1, 1);
-  CHECK_GT(2, 1);
-  CHECK_GT(2, 2);
-}
